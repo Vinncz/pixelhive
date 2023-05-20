7d277f2090cc5865c56d736b896b1cc97fd0581a
Vin <kevgun03@gmail.com>
2023-05-20 22:05:09 +0700

    login page first draft complete

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head';
 
 export default function NavBar () {
     return (
-        <div id='navbar' className="flex fullW r pagePadding centerVerti spaceBetweenH padtop15 padtom15 z1"> 
+        <div id='navbar' className="flex fullW r pagePadding centerVerti spaceBetweenH padtop15 padtom15 z1">
             <div className="flex r centerVerti gap5 marleft-5">
                 {/* <svg width="25" height="25" viewBox="0 0 40 40" className='marri15' fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g clip-path="url(#clip0_85_1600)">
@@ -47,20 +47,20 @@ export default function NavBar () {
 
                 <span id="logo">
                     <Link href='/' className='em1_5 b'>
-                        PixelHive™ 
+                        PixelHive™
                     </Link>
                 </span>
             </div>
             <div className="flex marri-10 gap10">
                 <span id="sign-in-button">
-                    <Link href='/'>
+                    <Link href='/auth/login'>
                         <div className="coloredEl pad10 padleft25 padri25 boxedEl1 borrad5">
                             Sign In
                         </div>
                     </Link>
                 </span>
                 <span id="register-button">
-                    <Link href='/'>
+                    <Link href='/auth/register'>
                         <div className="coloredEl pad10 padleft25 padri25 boxedEl1 borrad5">
                             Register
                         </div>
---
+++ lib/fetch/auth/login.tsx
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Make a request to your Laravel API to authenticate or perform other actions
+    const response = await fetch('http://localhost:8000/api/auth/login', {
+        method: 'GET',
+        headers: {
+            'username': "Vinn",
+            'password': "abc",
+        },
+        body: JSON.stringify({ username: 'your-username', password: 'your-password' }),
+    });
+
+    // Check if the request was successful
+    if (response.ok) {
+        // Retrieve the cookie from the Laravel API response
+        const cookie = response.headers.get('set-cookie');
+
+        // Assign the cookie in the Next.js response
+        res.setHeader('Set-Cookie', cookie ? cookie : "");
+        res.status(200).end();
+    } else {
+        // Handle unsuccessful request
+        res.status(response.status).json({ error: 'Login failed' });
+    }
+}
